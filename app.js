32854d84d6685ad4749e8b47af8d49c1583b58ae
Insider <insider@FVFWG4HDHV2D.local>
2018-12-20 12:11:08 +0100

    oty changee

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const http = require('http');
 const port=process.env.PORT || 3000
 
-
+/*
 const bot = new SlackBot(
   {
   token: 'xoxb-251748679685-508062977508-U5jQnpdnNALZeLleimTIjtDo' ,
   name: ''}
 );
-
+*/
 
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
