e8d13a80377583ed801a547aeaae39397bfd01ad
Insider <insider@FVFWG4HDHV2D.local>
2018-12-20 13:23:32 +0100

    oty try

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const http = require('http');
 const port=process.env.PORT || 3000
 
 
-const bot = new SlackBot(
-  {
-  token: 'xoxb-251748679685-508062977508-U5jQnpdnNALZeLleimTIjtDo',
-  name: 'mybot'
-});
 
 
 const server = http.createServer((req, res) => {
+
+
+  const bot = new SlackBot(
+    {
+    token: 'xoxb-251748679685-508062977508-U5jQnpdnNALZeLleimTIjtDo',
+    name: 'mybot'
+  });
+
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello oty :-) World</h1>');
