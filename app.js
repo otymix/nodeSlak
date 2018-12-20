2ef16eeba1ba9d1d4ac90af5108144f263a6832b
Insider <insider@FVFWG4HDHV2D.local>
2018-12-20 12:09:03 +0100

    oty change

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
 const http = require('http');
 const port=process.env.PORT || 3000
+
+
+const bot = new SlackBot(
+  {
+  token: 'xoxb-251748679685-508062977508-U5jQnpdnNALZeLleimTIjtDo' ,
+  name: ''}
+);
+
+
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+res.end('<h1>Hello oty World</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
